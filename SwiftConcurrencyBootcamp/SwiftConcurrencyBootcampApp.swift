c0f5774ad590af8d646b3def7696dd9f58ec6ae8
Chris Walter <walt50@gmail.com>
2023-08-31 14:52:58 -0400

    Initial Commit

---
+++ SwiftConcurrencyBootcamp/SwiftConcurrencyBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftConcurrencyBootcampApp.swift
+//  SwiftConcurrencyBootcamp
+//
+//  Created by Christopher Walter on 8/31/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftConcurrencyBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
